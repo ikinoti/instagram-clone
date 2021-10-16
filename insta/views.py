d03b3424820bb0cb2e17aaf48da9757e16c21400
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-16 12:41:38 +0300

    post creating view

--- insta/views.py
+++ insta/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 from django.views.generic import (
-  ListView
+  ListView,
+  CreateView,
+  DetailView
 )
 # Create your views here.
 
 class PostListView(ListView):
   template_name = 'insta/post_list.html'
-  queryset = Post.objects.all()
+  queryset = Post.objects.all().filter(created_date__lte=timezone.now()).order_by('-created_date')
   context_object_name = 'posts'
+
+class PostCreateView(CreateView):
+  template_name = 'insta/post_create.html'
+  form_class = PostForm
+  queryset = Post.objects.all()
+  success_url = '/'
+
+  def form_valid(self, form):
+    print(form.cleaned_data)
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
