9b2d71fc978bf2ff884bb0f8c0a63b050bbc6c50
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-10-19 13:51:59 +0300

    modified to accommodate cloudinary

--- insta/models.py
+++ insta/models.py
@@ -1,10 +1,12 @@
+from cloudinary import CloudinaryImage
 from django.db import models
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Post(models.Model):
   author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-  image = models.ImageField(default='default.png', blank=True, null=True)
+  image = CloudinaryField('image', default='default.png')
   caption = models.TextField()
   created_date=models.DateTimeField(default=timezone.now)
 
